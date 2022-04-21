9e4f8f0388a66b220d9c9d37a28ad78771f015b6
israel <israeldillanes2@gmail.com>
2022-04-21 13:30:07 -0500

    prediseño

---
+++ static/js/cod.js
@@ -0,0 +1,12 @@
+let activar = document.querySelector('.bt-activate');
+let elemento = document.querySelector('.seccion')
+let desactivar = document.querySelector('.bt-salir')
+
+activar.addEventListener('click', () => {
+    elemento.classList.add('seccion-active');
+});
+
+desactivar.addEventListener('click', () => {
+    elemento.classList.remove('seccion-active')
+})
+console.log("HOLA MUCHO GUSTO")
