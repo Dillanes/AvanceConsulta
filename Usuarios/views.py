ea2900041144b621c57459e15a6dc1f3c8d3393c
israel <israeldillanes2@gmail.com>
2022-04-21 15:47:16 -0500

    datos cargados correctamente

--- Usuarios/views.py
+++ Usuarios/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from .models import Omc23Nivel1
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    todoE = Omc23Nivel1.objects.all()
+    return render(request,'home.html',{'Empleados':todoE})
 
 def listaUsuarios(request):
     return render(request,'TablaUsuarios.html')
